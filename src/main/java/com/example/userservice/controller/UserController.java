fed27591eec3f78ff73989f0421831642de2f57b
TomyRavn <godqwer123@naver.com>
2023-03-17 15:19:26 +0900

    config-service 연동

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -35,8 +35,12 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String status() {
-        return String.format("It's working in User Service on PORT %s.",
-                env.getProperty("local.server.port"));
+        return String.format("It's working in User Service"
+                + ", port(local.server.port) = " + env.getProperty("local.server.port")
+                + ", port(server.port) = " + env.getProperty("server.port")
+                + ", token secret = " + env.getProperty("token.secret")
+                + ", token expiration time = " + env.getProperty("token.expiration_time")
+        );
     }
 
     @GetMapping("/welcome")
